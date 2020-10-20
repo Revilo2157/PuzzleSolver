44f3e02a13d2c290e212b5b456d797813b335260
Oliver Rodas <44071910+Revilo2157@users.noreply.github.com>
2020-10-20 01:12:06 +0000

    Added Edges and Started Enum

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-import PIL
+from PIL import Image, ImageFilter
 import cv2
+from enum import Enum
+
+def piece()
+
+puzzle = Image.open("puzzle3.png")
+pPix = puzzle.load()
+
+mask = Image.new("RGBA", (puzzle.size[0], puzzle.size[1]))
+blackWhite = mask.load()
+for x in range(puzzle.size[0]):
+  for y in range(puzzle.size[1]):
+    if pPix[x, y] != pPix[0, 0]:
+      blackWhite[x, y] = (255, 255, 255)
+    else:
+      blackWhite[x, y] = (0, 0, 0)
+  print(x)
+
+edges = mask.filter(ImageFilter.FIND_EDGES).convert("L")
+edges.save("edges.png")
+mask.save("mask.png")
